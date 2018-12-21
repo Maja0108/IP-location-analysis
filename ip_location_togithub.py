5073b5303ada5733fe00e9e86a95681182384816
Maja0108 <tajnacska@gmail.com>
2018-12-21 14:25:35 +0100

    new map
    Adding a new clustering map

--- ip_location_togithub.py
+++ ip_location_togithub.py
@@ -11,6 +11,7 @@ This code visualise the location of the IP adresses.
 import pandas as pd
 import seaborn as sns
 import folium
+from folium.plugins import MarkerCluster
 
 ip_location = pd.DataFrame()
 
@@ -28,7 +29,7 @@ g = sns.catplot(y='country', kind = 'count', data = ip_location)
 g.set(xscale = 'log')
 g.savefig('distribution.jpg')
 
-#Visualisation of the loactions in a map
+#Visualisation of the locations in a map
 ip_location_noNan =  ip_location.drop(['message'], axis = 1).dropna()
 
 m = folium.Map(location = [20, 0], tiles = "Mapbox Bright", zoom_start = 2)
@@ -36,3 +37,13 @@ for i in range(0, len(ip_location_noNan)):
     folium.Marker([ip_location_noNan.iloc[i]['lat'], ip_location_noNan.iloc[i]['lon']]).add_to(m)
     
 m.save('ip_location.html')
+
+# Cluster the markers and change the markerts to circle
+m2 = folium.Map(location = [20, 0], tiles = "Mapbox Bright", zoom_start = 2)
+marker_cluster = MarkerCluster().add_to(m2)
+
+for i in range(0, len(ip_location_noNan)):
+    folium.CircleMarker(location = [ip_location_noNan.iloc[i]['lat'], ip_location_noNan.iloc[i]['lon']], 
+                        radius = 5, fill_opacity = 0.9).add_to(marker_cluster)
+m2.save('ip_location2.html')  
+
